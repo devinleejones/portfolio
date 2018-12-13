3a8a086378c5d2a5371e81744ea372e4a4fa9360
Devin Lee-Jones <devinleejones@gmail.com>
2018-12-13 02:02:48 -0800

    Added WOWjs and Animate css into nav and header.

--- src/navbar.js
+++ src/navbar.js
@@ -5,25 +5,45 @@ export default class Navbar extends Component {
   render() {
     return (
       <nav>
-        <a href="https://devinleejones.github.io/portfolio/">Devin Lee-Jones</a>
+        <a
+          className="wow rollIn delay-1s"
+          href="https://devinleejones.github.io/portfolio/">
+          Devin Lee-Jones
+        </a>
         <ul>
           <li>
-            <a target="_blank" rel="noopener noreferrer" href={pdfFile}>
+            <a
+              className="wow fadeIn delay-1s"
+              target="_blank"
+              rel="noopener noreferrer"
+              href={pdfFile}>
               About Me
             </a>
           </li>
           <li>
-            <a target="_blank" rel="noopener noreferrer" href={pdfFile}>
+            <a
+              className="wow fadeIn delay-1s"
+              target="_blank"
+              rel="noopener noreferrer"
+              href={pdfFile}>
               Projects
             </a>
           </li>
           <li>
-            <a target="_blank" rel="noopener noreferrer" href={pdfFile}>
+            <a
+              className="wow fadeIn delay-1s"
+              target="_blank"
+              rel="noopener noreferrer"
+              href={pdfFile}>
               Tech
             </a>
           </li>
           <li>
-            <a target="_blank" rel="noopener noreferrer" href={pdfFile}>
+            <a
+              className="wow fadeIn delay-1s"
+              target="_blank"
+              rel="noopener noreferrer"
+              href={pdfFile}>
               Resume
             </a>
           </li>
